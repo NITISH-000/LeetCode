ce51400990c78f1bd973572d06eac8466f8e4fa4
Nitish Bhardwaj <nitishbhardwaj.nb7@gmail.com>
2026-07-15 20:23:02 +0530

    Time: 5 ms (80.69%), Space: 46 MB (53.65%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int rev = 0;
+        int temp = x;
+        while(temp != 0){
+            rev = rev*10 + temp%10;
+            temp = temp/10;
+        }
+        if(rev == x && x>=0){
+            return true;
+        }else if(x<0){
+            return false;
+        }else{
+            return false;
+        }
+    }
+}
