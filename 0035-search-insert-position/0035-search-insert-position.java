853b982ea4929076ab1ecc0a6610392299e55d73
Nitish Bhardwaj <nitishbhardwaj.nb7@gmail.com>
2026-07-15 21:02:42 +0530

    Time: 0 ms (100%), Space: 44.9 MB (28.85%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,6 +1,6 @@
 class Solution {
-    int x;
     public int searchInsert(int[] nums, int target) {
+        int x = 0;
         for(int i = 0; i < nums.length; i++){
             if(nums[i] >= target){
                  x = i;
