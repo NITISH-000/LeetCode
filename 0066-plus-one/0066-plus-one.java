8291ea510ce26622d73348c399a43b61816d30c0
Nitish Bhardwaj <nitishbhardwaj.nb7@gmail.com>
2026-07-22 00:02:51 +0530

    Time: 0 ms (100%), Space: 43.5 MB (43.26%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -4,16 +4,12 @@ class Solution {
             if(digits[i] == 9){
                 digits[i] = 0;
             }else{
-                digits[i] += 1;
-                break;
+                digits[i] ++;
+                return digits;
             }
         }
-        if(digits[0] == 0){
-            int[] arr = new int[digits.length+1];
-            arr[0] = 1;
-            return arr;
-        }else{
-            return digits;
-        }
+        int[] arr = new int[digits.length+1];
+        arr[0] = 1;
+        return arr;
     }
 }
